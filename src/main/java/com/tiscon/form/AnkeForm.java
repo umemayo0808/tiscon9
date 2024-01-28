2a4337d62dda1d7892d8b105539695ca5196a170
Syunya Hirokawa <shun_hr_711_0@icloud.com>
2024-01-28 04:58:54 +0000

    anke

---
+++ src/main/java/com/tiscon/form/AnkeForm.java
@@ -0,0 +1,57 @@
+package com.tiscon.form;
+
+
+
+
+public class AnkeForm {
+
+    private String manzoku;
+
+    public String getManzoku() {
+        return manzoku;
+    }
+
+    public void setManzoku(String manzoku) {
+        this.manzoku = manzoku;
+    }
+    
+    private String use;
+
+    public String getUse() {
+        return use;
+    }
+
+    public void setUse(String use) {
+        this.use = use;
+    }
+    
+    private String look;
+
+    public String getLook() {
+        return look;
+    }
+
+    public void setLook(String look) {
+        this.look = look;
+    }
+
+    private String reuse;
+
+    public String getReuse() {
+        return reuse;
+    }
+
+    public void setReuse(String reuse) {
+        this.reuse = reuse;
+    }
+
+    private String elsee;
+
+    public String getElsee() {
+        return elsee;
+    }
+
+    public void setElse(String elsee) {
+        this.elsee = elsee;
+    }
+}
